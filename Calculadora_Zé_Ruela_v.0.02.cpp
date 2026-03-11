87c6a10eb23d9c1e8f3c0c49ded7195e89b258f2
GabrielCarretts <gabrielcarretts@gmail.com>
2026-03-11 14:57:48 -0300

    feat: add initial Zé Ruela calculator with basic arithmetic

---
+++ Calculadora_Zé_Ruela_v.0.02.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <clocale>
+using namespace std;
+int main ( )
+
+{
+	setlocale (LC_ALL, "Portuguese");
+	
+	//Variáveis agrupadas
+	float n1, n2, resultado;
+	char operacao, resposta;
+	string nome_operacao, cor_da_fonte;
+	
+	do {
+	// 1. Setup e Boas-vindas
+	cout << "\n=====Bem vindo à Calculadora Zé Ruela v.0.02=====\n\n";
+	
+	//2. Coleta dos inputs (Entrada)
+	cout << "Digite o primeiro número: ";	
+	cin >> n1;
+	
+	cout << "Selecione a operação (+, -, *, /): ";
+	cin >> operacao;
+	
+	cout << "Digite o segundo número: ";
+	cin >> n2;
+	 
+	//4. Processamento (Operações matemáticas)	
+	if (operacao == '+') {
+		resultado = n1 + n2;
+		nome_operacao = "adição"; 
+    }
+    
+    	else if (operacao == '-') {
+		resultado = n1 - n2;
+		nome_operacao = "subtração";
+    }
+	
+		else if (operacao == '*') {
+		resultado = n1 * n2;
+		nome_operacao = "multiplicação";
+    }
+    
+    	else if (operacao == '/') {
+		resultado = n1 / n2;
+		nome_operacao = "divisão";
+    }
+	
+	//3. Saída
+	if (resultado > 0) {
+	cor_da_fonte = "\033[32m";
+	}
+	else if (resultado < 0) {
+	cor_da_fonte = "\033[31m" ;
+	}
+	else if (resultado == 0) {
+	cor_da_fonte = "\033[0m";
+	}
+
+	cout << "\nResultado da " << nome_operacao << ": " << cor_da_fonte << resultado << "\033[0m\n";
+	cout << "\nDeseja fazer outra operação? (S/N) ";
+	cin >> resposta;
+	} while (resposta == 's' || resposta == 'S');
+	
+	system("pause");
+	
+	return 0;
+}
